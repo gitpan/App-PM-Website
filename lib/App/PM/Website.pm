53ef9453148d35f2d034bca42fc810145aebd682
Andrew Grangaard <granny-publiccpan@ofb.net>
2012-08-29 06:32:59 +0000

    Import of SPAZM/App-PM-Website-0.122420 from CPAN.
    gitpan-cpan-distribution: App-PM-Website
    gitpan-cpan-version:      0.122420
    gitpan-cpan-path:         SPAZM/App-PM-Website-0.122420.tar.gz
    gitpan-cpan-author:       SPAZM
    gitpan-cpan-maturity:     released

---
+++ lib/App/PM/Website.pm
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+package App::PM::Website;
+{
+  $App::PM::Website::VERSION = '0.122420';
+}
+#use App::Cmd::Setup -app;
+use base 'App::Cmd';
+
+
+#ABSTRACT: tools for maintaining the a perl mongers (webDav) website
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+App::PM::Website - tools for maintaining the a perl mongers (webDav) website
+
+=head1 VERSION
+
+version 0.122420
+
+=head1 AUTHOR
+
+Andrew Grangaard <spazm@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Andrew Grangaard.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
